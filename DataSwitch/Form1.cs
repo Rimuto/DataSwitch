1622d25412117ce4ab0884d52871959a13160e0f
Rimuto <52855504+Rimuto@users.noreply.github.com>
2020-04-06 22:22:17 +1000

    Добавьте файлы проекта.

---
+++ DataSwitch/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataSwitch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string strCmdText;
+            strCmdText = "/c date 02.08.2013";
+            System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+        }
+    }
+}
